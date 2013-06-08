7bcdecef131400821c97af71ac15730451007399
Chris Bolton <chris.bolton972gmail.com>
2013-06-07 22:53:44 -0700

    completed timetable loading, json display

--- res/scripts/func.js
+++ res/scripts/func.js
@@ -3,27 +3,35 @@
 $.getJSON('res/db/data.json', function(data) {
 
 	for (var i in data.rotations) {
-		if(data.rotations[i].date === todayDate){
+		if(data.rotations[i].date === today_date){
 			var current = data.rotations[i];
 			
-			//document.getElementById("date").innerHTML = current.date;
-			document.getElementById("today-blockday").innerHTML = "Day " + current.day;
-			document.getElementById("block1").innerHTML = current.block1;
-			document.getElementById("block2").innerHTML = current.block2;
-			document.getElementById("block3").innerHTML = current.block3;
-			document.getElementById("block4").innerHTML = current.block4;
-			
-			switch(current.type){
-			
-				case "0":
-					loadTimetable("0");
-				break;
+			if(current.type === "10"){
+				$('#error-toast').show();
 				
-				case "1":
-					loadTimetable("1");
-				break;
-			
+				document.getElementById("error-toast").innerHTML = "No School Today";
+			}else{
+				
+				//document.getElementById("date").innerHTML = current.date;
+				var block1 = current.block1;
+				var block2 = current.block2;
+				var block3 = current.block3;
+				var block4 = current.block4;
+				document.getElementById("today-blockday").innerHTML = "Day " + current.day;
+				document.getElementById("block1").innerHTML = block1;
+				document.getElementById("block2").innerHTML = block2;
+				document.getElementById("block3").innerHTML = block3;
+				document.getElementById("block4").innerHTML = block4;
+				
+				//document.getElementById("timetable1").innerHTML = current.block1;
+				//document.getElementById("timetable2").innerHTML = current.block2;
+				//document.getElementById("timetable3").innerHTML = current.block3;
+				//document.getElementById("timetable4").innerHTML = current.block4;
+				
+				loadTimetable(current.type, block1, block2, block3, block4);
+				
 			}
+			
 		}else{}
 	}
 
--- res/scripts/init.js
+++ res/scripts/init.js
@@ -1,30 +1,35 @@
 console.log("initializing...");
 	
+	$('#error-toast').hide();
+	
+	
 	var now = new Date();
 
 	var days = new Array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
 	var months = new Array('January','February','March','April','May','June','July','August','September','October','November','December');
 
 	var date = ((now.getDate()<10) ? "0" : "")+ now.getDate();
-
 	function fourdigits(number)	{
 		return (number < 1000) ? number + 1900 : number;
 	}
-
+	
 	today_weekday =  days[now.getDay()];
 	today_date = months[now.getMonth()] + " " + date + ", " + (fourdigits(now.getYear())) ;
-
 	document.getElementById('today-weekday').innerHTML = today_weekday;
 	document.getElementById('today-date').innerHTML = today_date;
 	
 //====================================================================
 
-	var todayDate = "2012-09-01";
+	console.log(today_date);
 	
-	
-	function loadTimetable(type){
-		$('#load').load('res/timetables/list.html #' + type, function() {
+	function loadTimetable(type, block1, block2, block3, block4){
+		$('#timetable-load').load('res/timetables/timetable-full.html #' + type, function() {
 		  console.log('Load was performed on ' + type);
+		  document.getElementById("timetable1").innerHTML = block1;
+		  document.getElementById("timetable2").innerHTML = block2;
+		  document.getElementById("timetable3").innerHTML = block3;
+		  document.getElementById("timetable4").innerHTML = block4;
+
 		});
 	}
 
