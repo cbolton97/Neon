4368fbe53b6ffcb2a4b2de3ce54fcd5b028f8c52
Chris Bolton <chris.bolton97@gmail.com>
2013-12-27 17:01:17 -0800

    Rebuilt iOS package.

--- platforms/ios/WS Timetable/Classes/MainViewController.m
+++ platforms/ios/WS Timetable/Classes/MainViewController.m
@@ -62,8 +62,11 @@
 }
 
 #pragma mark View lifecycle
+
 - (void)viewWillAppear:(BOOL)animated
 {
+    // View defaults to full size.  If you want to customize the view's size, or its subviews (e.g. webView),
+    // you can do so here.
 
     [super viewWillAppear:animated];
 }
--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,6 +1,20 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
+        "file": "plugins/org.apache.cordova.console/www/console-via-logger.js",
+        "id": "org.apache.cordova.console.console",
+        "clobbers": [
+            "console"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.console/www/logger.js",
+        "id": "org.apache.cordova.console.logger",
+        "clobbers": [
+            "cordova.logger"
+        ]
+    },
+    {
         "file": "plugins/org.apache.cordova.device/www/device.js",
         "id": "org.apache.cordova.device.device",
         "clobbers": [
@@ -19,20 +33,6 @@ module.exports = [
         "id": "org.apache.cordova.inappbrowser.InAppBrowser",
         "clobbers": [
             "window.open"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.console/www/console-via-logger.js",
-        "id": "org.apache.cordova.console.console",
-        "clobbers": [
-            "console"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.console/www/logger.js",
-        "id": "org.apache.cordova.console.logger",
-        "clobbers": [
-            "cordova.logger"
         ]
     }
 ]
